43b253f871a9c9a6a9d3e3f7ddfb964b2154548e
Lena Schimmel <lena.schimmel@ameno.de>
2017-05-17 19:16:28 +0200

    Perform template matching (not fully working)

--- src/visualize/visualize.cpp
+++ src/visualize/visualize.cpp
@@ -13,20 +13,26 @@
 using namespace cv;
 using namespace std;
 
-const int height = 32;
-const int width = 24;
+const int height = 24;
+const int width = 32;
 const int size = width * height;
 int PIXEL_SIZE = 10;
 
-vector<Point> getMaximumPoints(Mat data, int minValue, int maxCount, int minDistance);
+vector<Point2f> getMaximumPoints(Mat data, int minValue, int maxCount, int minDistance);
 
 int main (int argc, char **argv) {
-    Mat data = Mat::zeros(height, width, CV_16SC1);
+    Mat data = Mat::zeros(32, 32, CV_16SC1);
 	Mat image = Mat::zeros(height * PIXEL_SIZE, width * PIXEL_SIZE, CV_8UC3);
 
     ifstream fs("pipe");
     string line, cell;
     getline(fs,line); // skip first line
+
+    vector<Point2f> horseTemplate;
+    horseTemplate.push_back(Point2f(13,16));
+    horseTemplate.push_back(Point2f(25,15)); 
+    horseTemplate.push_back(Point2f( 5,12));
+    horseTemplate.push_back(Point2f(25,10));
 
     while(getline(fs,line)) {
         stringstream lineStream(line);
@@ -34,10 +40,10 @@ int main (int argc, char **argv) {
         getline(lineStream, cell, ','); // skip second column
 
         // read data from stream into matrix
-        for(int x = 0; x < width; x++) {
-            for(int y = 0; y < height; y++) {
+        for(int y = 0; y < height; y++) {
+            for(int x = width - 1; x >= 0; x--) {
                 if(getline(lineStream,cell, ',')) {
-                    data.at<short>(x,y) = stoi(cell);
+                    data.at<short>(x, y) = stoi(cell);
                 } else {
                     data.at<short>(x, y) = 0;
                 };
@@ -57,7 +63,7 @@ int main (int argc, char **argv) {
         int absmax = abs(min) > abs(max) ? abs(min) : abs(max);
 
         // find maximum points
-        vector<Point> points = getMaximumPoints(data, 100, 10, 4);
+        vector<Point2f> points = getMaximumPoints(data, 100, 10, 4);
 
         // create visible image
         CvScalar color;
@@ -85,11 +91,18 @@ int main (int argc, char **argv) {
             }
         }
 
-        Point halfPixel(PIXEL_SIZE / 2, PIXEL_SIZE / 2);
-        cout << endl;
-        for(Point p : points) {
+        Point2f halfPixel(PIXEL_SIZE / 2, PIXEL_SIZE / 2);
+        //cout << endl;
+        for(Point2f p : points) {
             circle(image, p * PIXEL_SIZE + halfPixel, 16, CvScalar(0,0,255,255), 3);
-            cout << data.at<short>(p.x, p.y) << " ";
+            //cout << "(" << p.x << "," << p.y << ") ";
+        }
+        if(points.size() == 4) {
+            int maxInlierDist = 3;
+            Mat hom = findHomography( Mat(points), Mat(horseTemplate), CV_RANSAC, maxInlierDist);
+            float scaleX = norm(hom.col(0));
+            float scaleY = norm(hom.col(1));
+            cout << scaleX << " x " << scaleY << endl;
         }
         
         imshow("Raw Data", image);
@@ -98,9 +111,9 @@ int main (int argc, char **argv) {
     waitKey(0);
 }
 
-vector<Point> getMaximumPoints(Mat data, int minValue, int maxCount, int minDistance)
+vector<Point2f> getMaximumPoints(Mat data, int minValue, int maxCount, int minDistance)
 {
-    vector<Point> points;
+    vector<Point2f> points;
     Point maxPoint;
     for(int i = 0; i < maxCount; i++) {
         int max = 0;    
@@ -108,8 +121,8 @@ vector<Point> getMaximumPoints(Mat data, int minValue, int maxCount, int minDist
             for(int y = 0; y < height; y++) {
                 int v =  data.at<short>(x,y);
                 if(v > max) {
-                    Point newPoint(x,y);
-                    for(Point p : points)
+                    Point2f newPoint(x,y);
+                    for(Point2f p : points)
                         if(norm(p - newPoint) < minDistance)
                             goto nextPoint;
                     max = v;
