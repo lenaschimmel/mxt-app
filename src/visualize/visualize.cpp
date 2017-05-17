f1c59b819704914ece34657272ca8f6e7a414918
Lena Schimmel <lena.schimmel@ameno.de>
2017-05-17 18:20:07 +0200

    Restructured the code, detect max values and show them.

--- src/visualize/visualize.cpp
+++ src/visualize/visualize.cpp
@@ -18,72 +18,111 @@ const int width = 24;
 const int size = width * height;
 int PIXEL_SIZE = 10;
 
-int main (int argc, char **argv) {
-    Mat image = Mat::zeros(height * PIXEL_SIZE, width * PIXEL_SIZE, CV_8UC3);
-	
-    int data_buf[size];
+vector<Point> getMaximumPoints(Mat data, int minValue, int maxCount, int minDistance);
 
+int main (int argc, char **argv) {
+    Mat data = Mat::zeros(height, width, CV_16SC1);
+	Mat image = Mat::zeros(height * PIXEL_SIZE, width * PIXEL_SIZE, CV_8UC3);
 
     ifstream fs("pipe");
-    string line;
+    string line, cell;
     getline(fs,line); // skip first line
-    
-
-    
-    string                cell;
 
     while(getline(fs,line)) {
-        cout << "Read a line..." << endl;
         stringstream lineStream(line);
-        getline(lineStream,cell, ','); // skip first column
-        getline(lineStream,cell, ','); // skip second column
-        int i = 0;
-        while(getline(lineStream,cell, ','))
-        {
-            //cout << "V:'" << cell << "'" << endl;
-            data_buf[i++] = stoi(cell);
+        getline(lineStream, cell, ','); // skip first column
+        getline(lineStream, cell, ','); // skip second column
+
+        // read data from stream into matrix
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                if(getline(lineStream,cell, ',')) {
+                    data.at<short>(x,y) = stoi(cell);
+                } else {
+                    data.at<short>(x, y) = 0;
+                };
+            }
         }
 
+        // get min and max values
         int min = 0;
         int max = 0;
-        for (unsigned long i = 0; i < size; i++)
-        {
-            if(data_buf[i] < min) min = data_buf[i]; 
-            if(data_buf[i] > max) max = data_buf[i]; 
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                int v =  data.at<short>(x,y);
+                if(v < min) min = v; 
+                if(v > max) max = v;
+            }
         }
-        cout << "Min value: " << min;
-        cout << "Max value: " << max;
         int absmax = abs(min) > abs(max) ? abs(min) : abs(max);
 
+        // find maximum points
+        vector<Point> points = getMaximumPoints(data, 100, 10, 4);
+
+        // create visible image
         CvScalar color;
-        for (unsigned long i = 0; i < size; i++)
-        {
-            int x = i / height;
-            int y = i % height;
-
-            float it = float(data_buf[i]);
-            if(it < 0) 
-            {
-                it /= float(min);
-                if(it < 0.5)
-                    color = CvScalar(512 * it,0,0,255);
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                float it = data.at<short>(x, y);
+                if(it < 0) 
+                {
+                    it /= float(min);
+                    if(it < 0.5)
+                        color = CvScalar(512 * it, 0, 0, 255);
+                    else
+                        color = CvScalar(255, 512*(it -0.5), 512*(it - 0.5), 255);
+                }
                 else
-                    color = CvScalar(255,512*(it -0.5),512*(it -0.5),255);
-            }
-            else
-            {
-                it /= float(max);
-                if(it < 0.5)
-                    color = CvScalar(0,512 * it,0,255);
-                else
-                    color = CvScalar(512*(it -0.5),255,512*(it -0.5),255);
-            }
+                {
+                    it /= float(max);
+                    if(it < 0.5)
+                        color = CvScalar(0, 512 * it, 0, 255);
+                    else
+                        color = CvScalar(512 * (it - 0.5), 255, 512*(it - 0.5), 255);
+                }
 
-            rectangle(image, CvPoint(x * PIXEL_SIZE, y * PIXEL_SIZE), CvPoint( (x+1) * PIXEL_SIZE, (y+1) * PIXEL_SIZE), color, CV_FILLED);
+                rectangle(image, CvPoint(x * PIXEL_SIZE, y * PIXEL_SIZE), CvPoint( (x+1) * PIXEL_SIZE, (y+1) * PIXEL_SIZE), color, CV_FILLED);
+            }
+        }
+
+        Point halfPixel(PIXEL_SIZE / 2, PIXEL_SIZE / 2);
+        cout << endl;
+        for(Point p : points) {
+            circle(image, p * PIXEL_SIZE + halfPixel, 16, CvScalar(0,0,255,255), 3);
+            cout << data.at<short>(p.x, p.y) << " ";
         }
         
         imshow("Raw Data", image);
         waitKey(5);
     }
     waitKey(0);
+}
+
+vector<Point> getMaximumPoints(Mat data, int minValue, int maxCount, int minDistance)
+{
+    vector<Point> points;
+    Point maxPoint;
+    for(int i = 0; i < maxCount; i++) {
+        int max = 0;    
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                int v =  data.at<short>(x,y);
+                if(v > max) {
+                    Point newPoint(x,y);
+                    for(Point p : points)
+                        if(norm(p - newPoint) < minDistance)
+                            goto nextPoint;
+                    max = v;
+                    maxPoint = newPoint;
+                }
+                nextPoint: ;
+            }
+        }
+        if(max > minValue) {
+            points.push_back(maxPoint);
+        } else {
+            break;
+        }
+    }
+    return points;
 }
